bc605e20ebe546316dca965dc52bf1ac2a146c0d
erbol-mongol <erboleue@gmail.com>
2020-03-03 20:52:56 +0600

    started from zero

--- src/App.js
+++ src/App.js
@@ -2,33 +2,12 @@ import React from 'react';
 import {Route , BrowserRouter as Router} from 'react-router-dom';
 
 import './App.css';
-import Slider from './container/Slider/Slider';
-
-import * as ROUTES  from './constants/routes';
-import {withAuthentication} from './components/FirebaseComponents/Session'
-
-import Header from './container/Header/Header';
-import Main from './container/Main/Main';
-
 
 
 
 function App() {
   return (
-  <Router>
-    <div className="App">
-        <Header/>
-
-        <Route path = {ROUTES.SIGN_IN} render = {() => <div className = "amaOpacity"/>}/>
-        <Route path = {ROUTES.SIGN_UP} render = {() => <div className = "amaOpacity"/>}/>
-
-        <Route exact path ={ROUTES.LANDING}  render = {() => <Slider/>}/>
-        <Route exact path ={ROUTES.SIGN_IN}  render = {() => <Slider/>}/>
-        <Route exact path ={ROUTES.SIGN_UP}  render = {() => <Slider/>}/>
-        
-        <Main/>
-    </div>
-  </Router>
+    <h1>Hello World</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import Firebase, { FirebaseContext } from './container/Firebase/index';
 
 
-ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()}>
-      <App />
-    </FirebaseContext.Provider>,
-    document.getElementById('root'),
+ReactDOM.render(<App />,document.getElementById('root'),
   );
