26fca0d5483219393ee1a40d8764d0583d83468a
erbol-mongol <erboleue@gmail.com>
2020-03-14 20:27:10 +0600

    addedSomeMethods

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
     apiKey: "AIzaSyD6DuXhO-2zZQkkFSVb_zn20Xgz-4O85Nk",
@@ -12,8 +13,24 @@ const config = {
 
   class Firebase{
       constructor(){
-          app.initializeApp(config)
+          app.initializeApp(config);
+
+          this.auth = app.auth();
       }
+
+    doCreateUserWithEmailAndPassword = (email , password) =>
+        this.auth.createUserWithEmailAndPassword(email , password);
+
+    doSignInWithEmailAndPassword = (email , password) =>
+      this.auth.SignInWithEmailAndPassword (email , password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
+
   }
 
   export default Firebase;
